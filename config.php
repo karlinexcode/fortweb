14769bc534324ea0ab36239c0351915d635ae94c
Etini Archibong FXT <71902157+karlinexcode@users.noreply.github.com>
2022-04-27 09:29:33 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "syrexdig_SyrexDigitalAcademy";
+
+$conn = mysqli_connect($hostname, $username, $password, $database) or die(Database Connection Failure);;
+
+>
